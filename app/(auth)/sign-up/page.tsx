e37413a4adbc531970bf98f4bff2058f398d286b
kimjonghak <ggipk@naver.com>
2024-10-13 10:48:09 +0900

    로그인 회원 가입 까지

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+import React from 'react'
+import {SignUpCard} from "@/features/auth/componets/Sign-up-card";
+
+const SignUpPage = () => {
+    return (
+        <SignUpCard />
+    )
+}
+export default SignUpPage
