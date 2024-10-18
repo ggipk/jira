63e75902df9dcae63e015eb237945c63b693a54e
kimjonghak <ggipk@naver.com>
2024-10-18 17:23:19 +0900

    sidebar 했다

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+import { UserButton } from "@/components/user-button";
+import { Navbar } from "@/components/Navbar";
+
+export default function Home() {
+  return <div>This is Home page</div>;
+}
